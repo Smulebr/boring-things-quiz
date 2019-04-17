49bc9be910e537aff175de6c79dbb0474bb8a0c6
erogleva <e.rogleva@mail.bg>
2019-04-17 21:57:14 +0200

    Updates

---
+++ precache-manifest.cecd1a9b30e1c047f703f6d95c89430d.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7e8015d7d36dbeddbf704a64980dc58",
+    "url": "/boring-things-quiz/static/media/07263 Nähmaschine-min.d7e8015d.jpg"
+  },
+  {
+    "revision": "0ec7ddf078dd2210eec0",
+    "url": "/boring-things-quiz/static/css/main.466e1cf5.chunk.css"
+  },
+  {
+    "revision": "6ac487857c0fd96b96a3",
+    "url": "/boring-things-quiz/static/js/runtime~main.c0a4e59a.js"
+  },
+  {
+    "revision": "ae0b9170766c5eb6bd0ee54c717dee37",
+    "url": "/boring-things-quiz/static/media/torx-bits-metal-iron-60049.ae0b9170.jpeg"
+  },
+  {
+    "revision": "0141a4ab6cd27994b2f0",
+    "url": "/boring-things-quiz/static/js/2.ad5c60d5.chunk.js"
+  },
+  {
+    "revision": "a06c8e570353a4695a0211a0701686f6",
+    "url": "/boring-things-quiz/static/media/00581 Wappentafel-min.a06c8e57.jpg"
+  },
+  {
+    "revision": "548d96f8bcb3fe45930bc9b0694ac8d4",
+    "url": "/boring-things-quiz/static/media/02058 Fahne Stadtgarde-min.548d96f8.jpg"
+  },
+  {
+    "revision": "474956a0cd34e70ff70238b37b352eac",
+    "url": "/boring-things-quiz/static/media/02071 Kinderhändchen-min.474956a0.JPG"
+  },
+  {
+    "revision": "8667f69344655c0c36a10469d621795e",
+    "url": "/boring-things-quiz/static/media/02083-89 Schwörstäbe-min.8667f693.jpg"
+  },
+  {
+    "revision": "eb0a4dd63df6a19e25658de4c3b6d829",
+    "url": "/boring-things-quiz/static/media/02585_Fragmente Georgsbrunnen-min.eb0a4dd6.jpg"
+  },
+  {
+    "revision": "d9c9fb4e693698f06ac2871975645be1",
+    "url": "/boring-things-quiz/static/media/02908_2_Tabakspfeife-min.d9c9fb4e.jpg"
+  },
+  {
+    "revision": "cf61e858f4a95ab0df06be005a21c83f",
+    "url": "/boring-things-quiz/static/media/03485 Reutlinger Pokal-min.cf61e858.jpg"
+  },
+  {
+    "revision": "34000ced284405553055c4f3b7641057",
+    "url": "/boring-things-quiz/static/media/03486 Geldtruhe-min.34000ced.jpg"
+  },
+  {
+    "revision": "920fcb3e4336542004d21026a2f882d8",
+    "url": "/boring-things-quiz/static/media/03516_Minaturbuch-min.920fcb3e.JPG"
+  },
+  {
+    "revision": "ea39ae07a2538beead3ad6cc81a816de",
+    "url": "/boring-things-quiz/static/media/03642 Staubtuch Staatsbürgerinnen-min.ea39ae07.jpg"
+  },
+  {
+    "revision": "cf2a560104db7d938370882d3d97ed1f",
+    "url": "/boring-things-quiz/static/media/03751 Richtschwert-min.cf2a5601.jpg"
+  },
+  {
+    "revision": "044e5ac80fe7ed4a66219afab4c98ba6",
+    "url": "/boring-things-quiz/static/media/03766 Springerlemodel-min.044e5ac8.jpg"
+  },
+  {
+    "revision": "3b4f518fbe64b4afbb21aacec5d62372",
+    "url": "/boring-things-quiz/static/media/03843 Schandgeige-min.3b4f518f.jpg"
+  },
+  {
+    "revision": "a1276e08b2138d8639c24e95b84b0408",
+    "url": "/boring-things-quiz/static/media/03895_Beninhorn-min.a1276e08.jpg"
+  },
+  {
+    "revision": "d40f4acedffb851d2b322e4c65635a0b",
+    "url": "/boring-things-quiz/static/media/04729 Wappen Breuning-min.d40f4ace.jpg"
+  },
+  {
+    "revision": "7cfa1440d8c56ba5f73970115c46b3a1",
+    "url": "/boring-things-quiz/static/media/05337 Waschmaschine Zanker -min.7cfa1440.jpg"
+  },
+  {
+    "revision": "b2e000ce7be6be7801bc2ece0036dbbc",
+    "url": "/boring-things-quiz/static/media/05478 Zinnkanne-min.b2e000ce.jpg"
+  },
+  {
+    "revision": "8c046e9cfb51132a7bb0b5264e6f18f8",
+    "url": "/boring-things-quiz/static/media/05611_Nachtstuhl-min.8c046e9c.jpg"
+  },
+  {
+    "revision": "a8bb86754f6b43a39fe25f72a1a9cd42",
+    "url": "/boring-things-quiz/static/media/06413b Rechenmaschine-min.a8bb8675.jpg"
+  },
+  {
+    "revision": "0ec7ddf078dd2210eec0",
+    "url": "/boring-things-quiz/static/js/main.f45a6cb2.chunk.js"
+  },
+  {
+    "revision": "09773bdd8cd292a41c8d5d5614d77a10",
+    "url": "/boring-things-quiz/static/media/08302 Tübinger Pfennig-min.09773bdd.jpg"
+  },
+  {
+    "revision": "5ce79e9f20bb14e01c4c4744393115fa",
+    "url": "/boring-things-quiz/static/media/09177_T-Shirt_Nordtangente-min.5ce79e9f.jpg"
+  },
+  {
+    "revision": "9ee963ddfadfe9bc72c545e9a786cd9d",
+    "url": "/boring-things-quiz/static/media/09741 NS Reisenähzeug Cafe Pomana Judenfrei-min.9ee963dd.jpg"
+  },
+  {
+    "revision": "ca62d6da6af29625b67f0ba2439bc69d",
+    "url": "/boring-things-quiz/static/media/09765a Radio Zauberdose-min.ca62d6da.jpg"
+  },
+  {
+    "revision": "048e39eb7df555e1ac9da28fa8a17c14",
+    "url": "/boring-things-quiz/static/media/09858c Tübinger Stuhl-min.048e39eb.jpg"
+  },
+  {
+    "revision": "3c1e7ef9575af67885c3b21bca50a707",
+    "url": "/boring-things-quiz/static/media/10043 Bodenfliese-min.3c1e7ef9.jpg"
+  },
+  {
+    "revision": "64d4f18f5bfbcd3bcc310a2192715d4c",
+    "url": "/boring-things-quiz/static/media/10094 Dachziegel mit Sonnenmotiven-min.64d4f18f.jpg"
+  },
+  {
+    "revision": "6679f846b041aae7cfd29e3dc808f760",
+    "url": "/boring-things-quiz/static/media/10288 Zifferblatt Rathausuhr-min.6679f846.jpg"
+  },
+  {
+    "revision": "d338a1f23e863d700a384d98a8c8336e",
+    "url": "/boring-things-quiz/static/media/10421 Kassettendecke-min.d338a1f2.jpg"
+  },
+  {
+    "revision": "4c343db3a5e6512b2401403814f410b7",
+    "url": "/boring-things-quiz/static/media/11144 Teuerungstafel-min.4c343db3.jpg"
+  },
+  {
+    "revision": "52a5f8d09aeeeb1c2c5c8e38818c5d90",
+    "url": "/boring-things-quiz/static/media/11241 Kerzenhalter-min.52a5f8d0.jpg"
+  },
+  {
+    "revision": "1dd0faf81d4fd1ba12b33277cc7217f6",
+    "url": "/boring-things-quiz/static/media/11247 Teichel-min.1dd0faf8.jpg"
+  },
+  {
+    "revision": "935986e2d8913af09805a92587d539fd",
+    "url": "/boring-things-quiz/static/media/11248 Spitalzeichen-min.935986e2.jpg"
+  },
+  {
+    "revision": "54bddc6b2bcbef7e889c110f3528b1d2",
+    "url": "/boring-things-quiz/static/media/14080_Fenster_Dwengersche Buchhandlung-min.54bddc6b.jpg"
+  },
+  {
+    "revision": "63564d3b8caaf123b105b21bc8b631a0",
+    "url": "/boring-things-quiz/static/media/14089_Chorgestühl_Fürstenloge-min.63564d3b.jpg"
+  },
+  {
+    "revision": "7cd6419919f5792aca2c1969f8d92808",
+    "url": "/boring-things-quiz/static/media/14094 Jüdische_Gebetsriemen-min.7cd64199.jpg"
+  },
+  {
+    "revision": "896482a576212233c0678c73f00a3d41",
+    "url": "/boring-things-quiz/static/media/14095_Koffer_Wochenmark-min.896482a5.jpg"
+  },
+  {
+    "revision": "188390222a49a85525be58b6870c8a65",
+    "url": "/boring-things-quiz/static/media/3582 Küferwerkzeug-min.18839022.jpg"
+  },
+  {
+    "revision": "3bfc9d73ff3a6e5d2295d96bb19fb45b",
+    "url": "/boring-things-quiz/static/media/camera-vintage-lens-old.3bfc9d73.jpg"
+  },
+  {
+    "revision": "39a9f10f1d19b9763554ee3e9f53e750",
+    "url": "/boring-things-quiz/static/media/pexels-photo-1393319.39a9f10f.jpeg"
+  },
+  {
+    "revision": "9cbab354ab6e1eecd3c103681800f2bf",
+    "url": "/boring-things-quiz/static/media/pexels-photo-194101.9cbab354.jpeg"
+  },
+  {
+    "revision": "0141a4ab6cd27994b2f0",
+    "url": "/boring-things-quiz/static/css/2.00aa7633.chunk.css"
+  },
+  {
+    "revision": "aed6da9c3763edb1880c782194da448b",
+    "url": "/boring-things-quiz/index.html"
+  }
+];
